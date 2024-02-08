4b1dd689837185703b78c02598e394727ee51651
Dawid Mszanowski <dmszanowski@icloud.com>
2024-02-08 23:52:04 +0100

    Add working prediction

--- cli/commands/predict_command.py
+++ cli/commands/predict_command.py
@@ -1,6 +1,8 @@
 import click
 from services.weather_service import WeatherService
 import pandas as pd
+import os
+
 
 @click.command()
 @click.option(
@@ -8,7 +10,7 @@ import pandas as pd
     '-i',
     required=True,
     type=click.File('rb'),
-    help='Input CSV file.'
+    help='Input CSV file. The file should contain at least two columns: date and parameter to forecast. The date should be in the format dd.mm.yyyy. The parameter to forecast should be a numerical value. The file should be separated by semicolon.'
 )
 @click.option(
     '--output',
@@ -23,12 +25,18 @@ import pandas as pd
     help='Output directory.'
 )
 @click.option(
-    '--with-graphs',
-    '-wg',
-    required=False,
-    default=True,
-    type=click.BOOL,
-    help='Determinates whether to generate graphs.'
+    '--column-parameter',
+    '-cfp',
+    required=True,
+    type=click.STRING,
+    help='Name of the column in the input CSV file that contains the parameter to forecast. The parameter should be a numerical value.'
+)
+@click.option(
+    '--column-date',
+    '-cd',
+    required=True,
+    type=click.STRING,
+    help='Name of the column in the input CSV file that contains the date of the data to forecast. The date should be in the format dd.mm.yyyy.'
 )
 @click.option(
     '--periods-in-days',
@@ -36,19 +44,41 @@ import pandas as pd
     required=False,
     default=90,
     type=click.INT,
-    help='Number of periods in days.'
+    help='Number of periods in days. Default is 90 days.'
 )
-def predict(input, output, with_graphs, periods_in_days):
+@click.option(
+    '--with-plot',
+    '-wg',
+    required=False,
+    default=True,
+    type=click.BOOL,
+    help='Determinates whether to generate plot of predicted weather. The plot will be saved in the output directory as predicted_weather_[parameter].png. Default is True.'
+)
+@click.option(
+    '--with-csv',
+    '-wg',
+    required=False,
+    default=True,
+    type=click.BOOL,
+    help='Determinates whether to generate CSV of predicted weather. The CSV will be saved in the output directory as predicted_weather_[parameter].csv. Default is True.'
+)
+def predict(input, output, column_parameter, column_date, periods_in_days, with_plot, with_csv):
     """
-    Predicts the weather for a specific location. Takes a CSV file as input and returns CSV file and graph (if --with-graph is set to True) as output.
+    Predicts the weather for a specific location. Takes a CSV file as input and returns CSV file and graph as output.
     """
-    
-    df = pd.read_csv(input, delimiter=';')
-    df['datetime'] = pd.to_datetime(df['datetime'],format='%d.%m.%Y')
-    df = df[['datetime', 'temp']]
-    df.dropna(inplace=True)
 
     weather_service = WeatherService()
-    res = weather_service.predict_weather(df, periods_in_days)
+    dfRaw = pd.read_csv(input, delimiter=';')
+    df = weather_service.prepare_input_data(dfRaw, column_date, column_parameter)
+    res = weather_service.predict_weather(df, periods_in_days, with_plot)
+
+    if not os.path.exists(output):
+        os.makedirs(output)
+
+    if with_plot:
+        res['plot_figure'].savefig(output + '/predicted_weather_' + column_parameter + '.png')
+
+    if with_csv:
+        res['forecast'].to_csv(output + '/predicted_weather_' + column_parameter + '.csv', index=False)
 
     click.echo(res)
--- services/weather_service.py
+++ services/weather_service.py
@@ -1,11 +1,39 @@
 from neuralprophet import NeuralProphet
+import matplotlib.pyplot as plt
+import pandas as pd
+
 
 class WeatherService:
-    def predict_weather(self, df, periodsInDays):
+    def prepare_input_data(self, df, column_date, column_parameter):
+        df = df[[column_date, column_parameter]]
         df.dropna(inplace=True)
+        df[column_date] = pd.to_datetime(df[column_date], format='%d.%m.%Y')
+        return df
+
+    def predict_weather(self, df, periods_in_days, with_plot):
+        parameter_to_forecast = df.columns[1]
         df.columns = ['ds', 'y']
         model = NeuralProphet(quantiles=[0.05, 0.95])
         model.fit(df)
-        future = model.make_future_dataframe(df, periods=periodsInDays)
+        future = model.make_future_dataframe(df, periods=periods_in_days)
         forecast = model.predict(future)
-        return forecast
+
+        plot_figure = None
+        if with_plot:
+            model.plot(forecast)
+            model.plot_components(forecast)
+            model.plot_parameters()
+
+            plot_figure = plt.figure(figsize=(10, 6))
+            plt.plot(df['ds'], df['y'], label='Input data')
+            plt.plot(forecast['ds'], forecast['yhat1'],
+                     label='Predicted data', color='red')
+            plt.xlabel('Date')
+            plt.ylabel("Value")
+            plt.title('Prediction of ' + parameter_to_forecast + ' parameter')
+            plt.legend()
+
+        return {
+            'forecast': forecast,
+            'plot_figure': plot_figure,
+        }
